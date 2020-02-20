fe3cea451636c5452b8bbb03a29c12fc744ca2e0
Seth Wheeler <wheeler.seth@gmail.com>
2020-02-20 14:50:42 -0700

    intializes departments method and controller, initial route works

---
+++ db/migrate/20200220214611_create_departments.rb
@@ -0,0 +1,11 @@
+class CreateDepartments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :departments do |t|
+      t.string :name
+      t.string :category
+      t.string :floor
+
+      t.timestamps
+    end
+  end
+end
