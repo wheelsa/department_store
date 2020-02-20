2362ad8b9b083a6f3a7f26b231cca00b8fd57b05
Seth Wheeler <wheeler.seth@gmail.com>
2020-02-20 15:00:06 -0700

    department DB seeded

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'departments#index'
+  resources :departments
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  Department.create(name: 'Jewelry', category:'Jewelry and Perfume', floor: "First Floor - Central")
+  Department.create(name: 'Perfume', category:'Jewelry and Perfume', floor: "First Floor - Central")
+  Department.create(name: 'Mens Clothing', category:'Clothing', floor: "Second Floor - NW corner")
+  Department.create(name: 'Womens Clothing', category:'Clothing', floor: "Second Floor - SW corner")
+  Department.create(name: 'Appliances', category:'Home Goods', floor: "Third Floor - Central")
+  Department.create(name: 'Shoes', category:'Shoes, Socks and Underwear', floor: "First Floor - SW corner")
+  Department.create(name: 'Underwear', category:'Shoes, Socks and Underwear', floor: "First Floor - NE corner")
